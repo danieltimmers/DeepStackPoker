12c5fd2ee69301d265647f15c23a384026e48ee1
Daniel Timmers <danieltimmers2@gmail.com>
2019-12-18 11:00:41 -0500

    started working of DeckOfCards

--- DSPJava/src/deepstack/DeckOfCards.java
+++ DSPJava/src/deepstack/DeckOfCards.java
@@ -1,17 +1,41 @@
 package deepstack;
 
+import java.util.*;
+
+//This file is the deck of cards from which the game is played
 public class DeckOfCards {
-    private String deckName;
 
-    DeckOfCards() {
-        this.deckName = "default";
+    private ArrayList<String> deck;
+
+    public DeckOfCards() {
+        String[] suite = { " of Clubs", " of Hearts", " of Diamonds", " of Spades" };
+        String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        this.deck = new ArrayList<String>();
+
+        for (String i : suite) {
+            for (String k : rank) {
+                deck.add(k + i);
+            }
+        }
     }
 
-    public String getName() {
-        return this.deckName;
+    /**
+     * this method contains the shuffle() method under the Collections Object
+     */
+    public void shuffleDeck() {
+        Collections.shuffle(this.deck);
     }
 
-    public void setName(String deckName) {
-        this.deckName = deckName;
+    public int getLength() {
+        return this.deck.size();
     }
+
+    public String getCard(int n) {
+        return deck.get(n);
+    }
+
+    public void burn() {
+        deck.remove(0);
+    }
+
 }
